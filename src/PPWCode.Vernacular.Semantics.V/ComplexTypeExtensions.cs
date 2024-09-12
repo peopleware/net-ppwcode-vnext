690120ead9661d9134b4cdc677c5c3c3a2426c11
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2024-09-12 08:36:59 +0200

    Rename and move ComplexTypeExtensions -> CivilizedObjectExtensions

---
+++ src/PPWCode.Vernacular.Semantics.V/ComplexTypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using PPWCode.Vernacular.Exceptions.V;
+
+namespace PPWCode.Vernacular.Semantics.V;
+
+public static class CivilizedObjectExtensions
+{
+    public static void CheckForWildExceptions(this ICivilizedObject? civilizedObject, CompoundSemanticException cse)
+    {
+        if (civilizedObject is not null)
+        {
+            cse.AddElement(civilizedObject.WildExceptions());
+        }
+    }
+}

4c8cccf64d04667de252595d1582f89a5f9f96ca
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2024-10-01 16:13:35 +0200

    Move IdGenerator to PPWCode.Vernacular.Persistence.V.Tests

---
+++ src/PPWCode.Vernacular.Persistence.V.Tests/IdGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace PPWCode.Vernacular.Persistence.V.Tests;
+
+public static class IdGenerator
+{
+    private static int _nextId = 1000000;
+
+    public static int NextId
+        => Interlocked.Increment(ref _nextId);
+}

23730b67158b5d42642b21c848bc0db4544cef42
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2025-03-03 09:12:13 +0100

    Add PPWCode AspNetCore libraries

---
+++ src/PPWCode.AspNetCore.Server.I/LinksContext.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2025 by PeopleWare n.v..
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace PPWCode.AspNetCore.Server.I;
+
+public abstract class LinksContext
+{
+    public const string VersionRouteParameter = "version";
+    public const string DefaultApiVersionFormat = "VVV";
+
+    protected LinksContext(
+        ApiVersion apiVersion,
+        string? apiVersionFormat = null)
+    {
+        ApiVersion = apiVersion;
+        ApiVersionFormat = apiVersionFormat ?? DefaultApiVersionFormat;
+    }
+
+    public ApiVersion ApiVersion { get; }
+
+    public string ApiVersionFormat { get; }
+
+    public void AddVersionToRouteParameters(IDictionary<string, object> routeValues)
+    {
+        if (!routeValues.ContainsKey(VersionRouteParameter))
+        {
+            routeValues.Add(VersionRouteParameter, ApiVersion.ToString(ApiVersionFormat));
+        }
+    }
+}

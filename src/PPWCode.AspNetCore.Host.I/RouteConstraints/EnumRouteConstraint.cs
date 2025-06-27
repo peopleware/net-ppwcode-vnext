d8e90d6a2839e9b175e095ff7a716ac066248c7e
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2025-06-27 10:31:33 +0200

    Add support for a route connstraint with a Enum datatype

---
+++ src/PPWCode.AspNetCore.Host.I/RouteConstraints/EnumRouteConstraint.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace PPWCode.AspNetCore.Host.I.RouteConstraints;
+
+public class EnumRouteConstraint<TEnum> : IRouteConstraint
+    where TEnum : struct, Enum
+{
+    /// <inheritdoc />
+    public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+    {
+        if (!values.TryGetValue(routeKey, out object? value) || value is null)
+        {
+            return false;
+        }
+
+        return Enum.TryParse<TEnum>(value.ToString(), true, out _);
+    }
+}
---
+++ src/PPWCode.AspNetCore.Host.I/RouteConstraints/RouteConstraintExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PPWCode.AspNetCore.Host.I.RouteConstraints;
+
+public static class RouteConstraintExtensions
+{
+    public static IServiceCollection AddEnumConstraint<TEnum>(this IServiceCollection services, string name)
+        where TEnum : struct, Enum
+    {
+        services.Configure<RouteOptions>(options => { options.ConstraintMap[name] = typeof(EnumRouteConstraint<TEnum>); });
+        return services;
+    }
+}

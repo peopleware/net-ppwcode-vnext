17f1413a4f606a997632d5307e912393298e351c
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2025-10-07 13:35:24 +0200

    Add ComparePropetry validation attribute, allow IComparable types to be check to each other

---
+++ src/PPWCode.AspNetCore.API.I/ComparisonTypeEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace PPWCode.AspNetCore.API.I;
+
+public enum ComparisonTypeEnum
+{
+    Equal,
+    NotEqual,
+    GreaterThan,
+    GreaterThanOrEqual,
+    LessThan,
+    LessThanOrEqual
+}
